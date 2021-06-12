78d095d00953181a67e18ddc7588e33f2ca882ab
Camden Bickel <26368233+camden@users.noreply.github.com>
2021-06-12 15:52:49 -0700

    Initial commit

---
+++ src/server/app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("dist"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../../public/index.html"));
+});
+
+var listener = app.listen(8080, function () {
+  console.log("Listening on port " + listener.address().port);
+});
